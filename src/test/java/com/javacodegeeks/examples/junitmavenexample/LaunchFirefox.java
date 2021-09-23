7548760a12ea69afe6c79552293bbf7b64627866
Sunil Saxena <sunil.saxena@alqimi.com>
2021-09-24 00:22:47 +0530

    changes

--- src/test/java/com/javacodegeeks/examples/junitmavenexample/LaunchFirefox.java
+++ src/test/java/com/javacodegeeks/examples/junitmavenexample/LaunchFirefox.java
@@ -4,6 +4,7 @@ package com.javacodegeeks.examples.junitmavenexample;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class LaunchFirefox {
@@ -12,7 +13,9 @@ public class LaunchFirefox {
 	 public void launch() {
 		System.out.println("launch browser");
 		System.setProperty("webdriver.chrome.driver", ".\\Exefiles\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--disable-gpu");
+		ChromeDriver driver = new ChromeDriver(chromeOptions);
 		driver.manage().window().maximize();
 		driver.navigate().to("https://www.flipkart.com/"); 
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
